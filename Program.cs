0714da2b289fe40ec175f11a424c999cea74c870
rokvair <rokas.vaitkunas@ktu.edu>
2026-04-19 19:20:42 +0300

    Fix HTTPS redirect for Azure deployment

--- Program.cs
+++ Program.cs
@@ -69,7 +69,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+    app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
